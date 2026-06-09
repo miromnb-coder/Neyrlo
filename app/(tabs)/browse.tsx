ceec76ea4affb563dcbfb0f8161de896fda68f15
miromnb-coder <miromnb@icloud.com>
2026-06-09 14:30:17 +0300

    Add browse tab placeholder

---
+++ app/(tabs)/browse.tsx
@@ -0,0 +1,11 @@
+import { ComingSoonScreen } from '@/components/ComingSoonScreen';
+
+export default function BrowseScreen() {
+  return (
+    <ComingSoonScreen
+      description="Seuraavaksi tänne lisätään kategoriat, suodattimet ja listausnäkymä tavaroiden selaamiseen ilman karttaa."
+      icon="grid-outline"
+      title="Selaa tavaroita"
+    />
+  );
+}
