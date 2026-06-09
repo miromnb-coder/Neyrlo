fbca896c29bae28d773c1d62eba7631527af8524
miromnb-coder <miromnb@icloud.com>
2026-06-09 14:30:46 +0300

    Add item creation tab placeholder

---
+++ app/(tabs)/add.tsx
@@ -0,0 +1,11 @@
+import { ComingSoonScreen } from '@/components/ComingSoonScreen';
+
+export default function AddItemScreen() {
+  return (
+    <ComingSoonScreen
+      description="Tänne rakennetaan lomake, jolla käyttäjä voi lisätä tavaran, kuvat, noutoalueen, hinnan tai lainaehdot."
+      icon="add-circle-outline"
+      title="Lisää tavara"
+    />
+  );
+}
